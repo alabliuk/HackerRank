9a59985e579e9e6a1b2f52077b5d05b96540fc3d
alabliuk <andrelabliuk@gmail.com>
2019-09-26 01:53:59 -0300

    Adding resolutions

---
+++ Python/Loops.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-loops/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    x = int(0)
+
+    while x < n:
+        print(x**2)
+        x += 1
---
+++ Python/Print_Function.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-print/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    x = int(1)
+
+    while x <= n:
+        print(x, end='')
+        x += 1
