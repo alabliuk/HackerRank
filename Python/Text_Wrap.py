d04ade6eec71fcb1f27eeb9237d02cd8fdc474db
AndreL1 <andre.leme@verifone.com>
2019-10-01 11:09:54 -0300

    textwrap

---
+++ Python/Text_Wrap.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/text-wrap/problem
+
+import textwrap
+
+def wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width=max_width) 
+    return wrapper.fill(text=string)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
