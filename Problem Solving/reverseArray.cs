a81d9cdd6cb2f29306e4ebbc944efcf12fc8b26d
alabliuk <andrelabliuk@gmail.com>
2021-01-19 20:10:39 -0300

    Add exercices

---
+++ Problem Solving/reverseArray.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System;
+
+class Solution
+{
+
+    // Complete the reverseArray function below.
+    static int[] reverseArray(int[] a)
+    {
+        int i = a.Length;
+        int[] _reverse = new int[i];
+
+        foreach (int x in a)
+        {
+            i--;
+            _reverse[i] = x;
+        }
+        return _reverse;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        int[] res = reverseArray(arr);
+
+        textWriter.WriteLine(string.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Problem Solving/rotateLeft.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<int> rotateLeft(int d, List<int> arr)
+    {
+        int[] _arr = new int[arr.Count];
+        int position = arr.Count - d;
+
+        foreach (int value in arr)
+        {
+            if (position < arr.Count)
+            {
+                _arr[position] = value;
+            }
+            else
+            {
+                position = position - arr.Count;
+                _arr[position] = value;
+            }
+
+            position++;
+        }
+
+        return _arr.Cast<int>().ToList();
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.rotateLeft(d, arr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
